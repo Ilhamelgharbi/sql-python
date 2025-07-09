848dbca0aa54f76ef668af1dc992381b067d4384
Ilham El Gharbi <129683687+Ilhamelgharbi@users.noreply.github.com>
2025-07-09 10:35:52 +0100

    Update connect.py

--- connect.py
+++ connect.py
@@ -3,3 +3,19 @@ from dotenv import  load_dotenv
 from  sqlalchemy import create_engine
 load_dotenv()
 
+DB_HOST = os.getenv(DB_HOST)
+DB_USER =  os.getenv(DB_USER)
+DB_PORT =  os.getenv(DB_PORT)
+DB_PASSWORD =  os.getenv(DB_PASSWORD)
+DB_DBNAME =  os.getenv(DB_DBNAME)
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}
+
+engine= create_engine(DATABASE_URL)
+
+
+try :
+  with engine.connect() as connection :
+    print("conection is successful")
+except exception as e :
+  print(e)
